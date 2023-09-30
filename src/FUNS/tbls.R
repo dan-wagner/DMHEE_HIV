b03572a8af21cc98ac5341afc4fb5c3ecb1fe64a
Daniel Wagner <wagnerd58@gmail.com>
2023-09-30 13:45:01 -0600

    Updated display tables

--- src/FUNS/tbls.R
+++ src/FUNS/tbls.R
@@ -3,21 +3,31 @@ adopt_tbl <- function(x,
                       lambda, 
                       nbType = "NMB", 
                       currency = "CAD"){
+  # Check if data supplied to `x` are deterministic
+  deterministic <- length(dim(x)) == 2
   
   # Perform Cost-Effectiveness Analysis
-  cea_result <- list(ia = NULL, nb = NULL)
-  cea_result$ia <- inc_analysis(data = x, effect_measure = effect_measure)
-  cea_result$nb <- nb_analysis(data = x,
-                               lambda = lambda,
-                               effect_measure = effect_measure, 
-                               nbType = nbType,
-                               show.error = TRUE)
-  # TODO: Add logic to restrict p_error to max(eNB)
-  # TODO: Add function to conduct a cost-effectiveness analysis
+  if (isTRUE(deterministic)) {
+    cea_result <- inc_analysis(data = x, effect_measure = effect_measure)
+    cea_result <- as.data.frame(cea_result)
+  } else {
+    cea_result <- list(ia = NULL, nb = NULL)
+    cea_result$ia <- inc_analysis(data = x, effect_measure = effect_measure)
+    cea_result$nb <- nb_analysis(data = x,
+                                 lambda = lambda,
+                                 effect_measure = effect_measure, 
+                                 nbType = nbType,
+                                 show.error = TRUE)
+    # TODO: Add function to conduct a cost-effectiveness analysis
+    
+    # Convert Output to Data Frame
+    cea_result <- lapply(X = cea_result, FUN = as.data.frame)
+    cea_result <- Reduce(f = cbind, x = cea_result)
+  }
   
-  # Convert Output to Data Frame
-  cea_result <- lapply(X = cea_result, FUN = as.data.frame)
-  cea_result <- Reduce(f = cbind, x = cea_result)
+  
+  
+  
   
   # Misc Tasks
   currency_sym <- switch(currency, 
@@ -54,29 +64,39 @@ adopt_tbl <- function(x,
                           locations = gt::cells_body(columns = c(ICER), 
                                                      rows = ExtDom == 1))
   TBL <- gt::cols_hide(data = TBL, columns = gt::contains("Dom"))
+  
   # Format: Net-Benefit Results
-  # Add Spanner Column for each lambda considered
-  for (i in lambda) {
-    spanner_lab <- paste0("\u03BB = ", 
-                          currency_sym, 
-                          paste(i, effect_measure, sep = "/"))
-    
-    TBL <- gt::tab_spanner(data = TBL, 
-                           label = spanner_lab, 
-                           columns = contains(as.character(i)))
-  }
-  # Apply Column Labels within each spanner column
-  TBL <- gt::cols_label(.data = TBL, 
-                        gt::contains(match = "eNB") ~ "NMB", 
-                        gt::contains(match = "prob_CE") ~ "P(CE)", 
-                        gt::contains(match = "p_error") ~ "P(Error)")
-  if (nbType == "NHB") {
+  # Applies to situations where supplied data are from deterministic simulation
+  if (isFALSE(deterministic)) {
+    # Add Spanner Column for each lambda considered
+    for (i in lambda) {
+      spanner_lab <- paste0("\u03BB = ", 
+                            currency_sym, 
+                            paste(format(x = i, big.mark = ","), 
+                                  sub(pattern = "s$", 
+                                      replacement = "", 
+                                      x = effect_measure), 
+                                  sep = "/"))
+      
+      TBL <- gt::tab_spanner(data = TBL, 
+                             label = spanner_lab, 
+                             columns = contains(as.character(i)))
+    }
+    # Apply Column Labels within each spanner column
     TBL <- gt::cols_label(.data = TBL, 
-                          gt::contains(match = "eNB") ~ "NHB")
+                          gt::contains(match = "eNB") ~ "NMB", 
+                          gt::contains(match = "prob_CE") ~ "P(CE)", 
+                          gt::contains(match = "p_error") ~ "P(Error)")
+    
+    if (nbType == "NHB") {
+      TBL <- gt::cols_label(.data = TBL, 
+                            gt::contains(match = "eNB") ~ "NHB")
+    }
+    TBL <- gt::sub_missing(data = TBL, 
+                           columns = contains(match = "p_error"), 
+                           missing_text = "---")
   }
-  TBL <- gt::sub_missing(data = TBL, 
-                         columns = contains(match = "p_error"), 
-                         missing_text = "---")
+  
   # Format: Currency and Numbers
   TBL <- gt::fmt_currency(data = TBL, 
                           columns = c("Costs", "ICER", gt::contains("NB")),
@@ -88,6 +108,18 @@ adopt_tbl <- function(x,
   # Add Title. 
   TBL <- gt::tab_header(data = TBL, 
                         title = "Cost-Effectiveness Results")
+  # Add Source Note: 
+  if (isTRUE(deterministic)) {
+    SNote <- paste("Data generated from deterministic simulation.")
+  } else {
+    SNote <- paste0("Data generated from a Monte Carlo simulation of ", 
+                    format(x = nrow(x), 
+                           big.mark = ",", 
+                           big.interval = 3L), 
+                    " iterations.")
+  }
+  TBL <- gt::tab_source_note(data = TBL, source_note = SNote)
+  
   # Format: Table Theme
   TBL <- HEE_theme(x = TBL)
   
@@ -111,8 +143,7 @@ HEE_theme <- function(x){
                        locations = list(gt::cells_title(groups = "title"), 
                                         gt::cells_title(groups = "subtitle")))
   
-  tbl <- gt::tab_options(data = tbl, 
-                         table_body.border.bottom.color = "black",
+  tbl <- gt::tab_options(data = tbl,
                          table.border.bottom.color = "black", 
                          table.border.top.color = "black")
   
